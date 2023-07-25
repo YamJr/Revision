719c3638d0ed1010814c94aaee3edf8de437a192
YamJr <yamgrg2058@gmail.com>
2023-07-25 11:56:17 +0545

    demo

---
+++ linqdemo/linqdemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linqdemo
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            int[] scores = new int[] { 1, 2, 3, 4, 5, };
+            IEnumerable<int> scoresQuerry = from score in scores    
+                                            where score > 0
+                                            select score;
+            foreach (int i in scoresQuerry)
+            {
+                Console.WriteLine(i + " ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ multiDelegates/multiDelegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace multiDelegates
+   
+{
+    public delegate void MyDelegate(string Message);
+    internal class Program
+    {
+        static void Main()
+        {
+            MyDelegate multicastDelegate = Method1;
+            multicastDelegate += Method2;
+            multicastDelegate += Method3;
+            multicastDelegate("Hello Everyone");
+            multicastDelegate -= Method2;
+            multicastDelegate("Hello There");
+         }
+            static void Method1(string message)
+            {
+                Console.WriteLine("Method1:" +message);
+            }
+            static void Method2(string message)
+            {
+                Console.WriteLine("Method2:" + message);
+            }
+            static void Method3(string message)
+            {
+                Console.WriteLine("Method3:" + message);
+            Console.ReadLine();
+            }
+    }
+}
