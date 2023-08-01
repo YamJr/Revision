1d0d3f5c5cea77d2be883e4f55ffa62ea9fa0a65
YamJr <yamgrg2058@gmail.com>
2023-08-01 21:19:23 +0545

    demo

---
+++ SealedClass/SealedClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SealedClass
+{
+    public sealed class Circle
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5.0);
+            Console.WriteLine("The area of circle is " + circle.CalculateArea());
+            Console.ReadLine();
+        }
+    }
+}
