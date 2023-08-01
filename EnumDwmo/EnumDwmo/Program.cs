507cb855814ac7bf50918296507f795a93252a33
YamJr <yamgrg2058@gmail.com>
2023-08-01 20:58:20 +0545

    demo

--- EnumDwmo/EnumDwmo/Program.cs
+++ EnumDwmo/EnumDwmo/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EnumDemo
 {
     internal class Program
     {
+        public enum Days
+        {
+            Sunday,
+            Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+         Saturday,
+        }
         static void Main(string[] args)
         {
+            Days today = Days.Saturday;
+            Console.WriteLine("Today is: " + today);
+            switch (today)
+            {
+                case Days.Sunday:
+                    Console.WriteLine("Its Sunday");
+                    break;
+                case Days.Monday:
+                    Console.WriteLine("Its Monday");
+                    break;
+                case Days.Tuesday:
+                    Console.WriteLine("Its Tuesday");
+                    break;
+                case Days.Wednesday:
+                    Console.WriteLine("Its Wednesday");
+                    break;
+                case Days.Thursday:
+                    Console.WriteLine("Its Thursday");
+                    break;
+                case Days.Friday:
+                    Console.WriteLine("Its Friday");
+                    break;
+                case Days.Saturday:
+                    Console.WriteLine("Its Saturday");
+                    break;
+                Default:
+                    Console.WriteLine("Invalid Day");
+                    break;
+
+            }
+            Console.ReadLine();
         }
     }
 }
